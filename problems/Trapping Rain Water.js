6101545240340656251a35779358ded0e0c71b22
Egor Grushin <egorgrushin@gmail.com>
2023-02-28 15:00:14 +0300

    init jest, babel and first problem

---
+++ problems/Trapping Rain Water.js
@@ -0,0 +1,25 @@
+export const trap = (heights) => {
+	let trapped = 0, descents = [], lastHeight = -1;
+
+	for (let i = 0; i < heights.length; i++) {
+		const height = heights[i];
+		// down
+		if (height < lastHeight) {
+			descents.push(i - 1);
+		}
+		// up
+		if (height >= lastHeight) {
+			for (let j = lastHeight + 1; j <= height; j++) {
+				const descent = descents.pop();
+				if (descent	 === undefined) continue;
+				const descentHeight = heights[descent];
+				const depth = Math.min(descentHeight, j) - lastHeight;
+				if (depth > 0) {
+					trapped += (i - descent - 1) * depth;
+				}
+			}
+		}
+		lastHeight = height;
+	}
+	return trapped;
+};
---
+++ problems/Trapping Rain Water.spec.js
@@ -0,0 +1,13 @@
+import { trap } from './Trapping Rain Water';
+
+describe('Trapping Rain Water', () => {
+	test.each`
+		input 									| output
+		${[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]} | ${6}
+		${[2, 0, 2]} 							| ${2}
+		${[2, 1, 0, 2]} 						| ${3}
+	`('$input should match $output', ({ input, output }) => {
+		const result = trap(input);
+		expect(result).toBe(output);
+	})
+});
